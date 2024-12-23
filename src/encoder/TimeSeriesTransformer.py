197514805af66e8158c4e5e7c56df74a4a257511
laurenzlrz <laurenz.wurzel@gmail.com>
2024-12-23 11:50:32 +0100

    Refactoring and added documentation in TimeSeriesTransformer.py

--- src/encoder/TimeSeriesTransformer.py
+++ src/encoder/TimeSeriesTransformer.py
@@ -1,29 +1,57 @@
-from encoder.EncoderBlock import EncoderBlock
-from encoder.PositionalEncoding import PositionalEncoding
 from torch import nn
 import torch
 
-class TimeSeriesTransformer(nn.Module):
-    #TODO Variable AttentionHeads und Anzahl Encoder Blocks
-    def __init__(self, input_size, head_sizes, num_blocks, output_size_attention):
+from src.encoder.EncoderStack import EncoderStack
+from src.encoder.PositionalEncoding import PositionalEncoding
 
+
+class TimeSeriesTransformer(nn.Module):
+    """
+    TimeSeriesTransformer is a neural network model for time series prediction.
+    It consists of an encoder stack and a linear layer for regression.
+
+    Attributes:
+        input_size (int): The size of the input tensor.
+        positional_encoding (PositionalEncoding): The positional encoding module.
+        encoder_stack (EncoderStack): The stack of encoder blocks.
+        regressor (nn.Linear): The linear layer for regression.
+    """
+
+    def __init__(self, input_size, head_sizes):
+        """
+        Initializes the TimeSeriesTransformer model.
+
+        Args:
+            input_size (int): The size of the input tensor.
+            head_sizes (List[List[int]]): The number of attention heads for each block in the encoder stack.
+        """
         super(TimeSeriesTransformer, self).__init__()
         self.input_size = input_size
-        self.encoder_blocks = nn.ModuleList([EncoderBlock(input_size, head_sizes, output_size_attention, input_size) for _ in range(num_blocks)])
-        self.encoder = nn.Sequential(*self.encoder_blocks)
-        self.regressor = nn.Linear(input_size, 1)
         self.positional_encoding = PositionalEncoding(self.input_size)
+        self.encoder_stack = EncoderStack(input_size, head_sizes)
+        self.regressor = nn.Linear(self.input_size, 1)
 
     def forward(self, x):
+        """
+        Forward pass for the TimeSeriesTransformer model.
+        Positional encoding is applied to the input tensor, then passed through the encoder stack.
+        The output is pooled and passed through the regressor.
+
+        Args:
+            x (Tensor): The input tensor.
+
+        Returns:
+            Tensor: The predicted output tensor.
+        """
         positional_encoded = self.positional_encoding(x)
-        encoded = self.encoder(positional_encoded)
+        encoded = self.encoder_stack(positional_encoded)
         pooled = encoded.mean(dim=-2)
         pred = self.regressor(pooled)
 
         return pred
 
-test_transformer = TimeSeriesTransformer(input_size=32, head_sizes=[16,16,16], num_blocks=2, output_size_attention=32)
 
+# Example usage
+test_transformer = TimeSeriesTransformer(32, [[16, 16, 16] for _ in range(0, 3)])
 test_tensor = torch.rand(8, 32)
-
 print(test_transformer(test_tensor))
